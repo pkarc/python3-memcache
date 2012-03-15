fd738de615334e11e05abaae70154f527719d633
Ady Liu <adyliu@sohu-inc.com>
2012-03-15 16:32:43 +0800

    add setup script

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(
+    name = 'python3-memcache',
+    packages = ['.'],
+    version = '1.0.0',
+    description = 'pure python(3.x) client for memcache',
+    author = 'Ady Liu',
+    author_email = 'imxylz@gmail.com',
+    download_url = 'http://github.com/adyliu/python3-memcache',
+    keywords = ['memcached','python3'],
+)
